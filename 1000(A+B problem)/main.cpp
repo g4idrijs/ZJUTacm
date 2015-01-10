8234b26ff6a5acb1c58b0d4bb83098db6c0d185e
王源果 <g4idrijs@gmail.com>
2015-01-10 15:23:43 +0800

    问题ID1000
    第一个问题

---
+++ 1000(A+B problem)/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+#define SIZE 9
+
+static int solution_count = 0;
+static char message[] = "the number of possible solution is 9999999:";
+int main(void)
+{
+ 	printf("the number of possible solution is %d:\n", solution_count);
+ 	cout<<"Hello World!"<<endl;
+	return 0;
+}
